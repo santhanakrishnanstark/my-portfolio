84edd5834149919ac85d0ffff3855dbac35d9005
santhanakrishnanstark <santhanakrishnanstark@gmail.com>
2021-12-31 11:05:48 +0530

    hero section setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import React from 'react';
 import './App.css';
+import Header from './Components/Header/Header';
+import HeroSection from './Components/HeroSection/HeroSection';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World from Dev</h1>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header />
+      <HeroSection />
+    </React.Fragment>
   );
 }
 
